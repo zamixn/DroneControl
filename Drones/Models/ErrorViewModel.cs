5f272575317bac8ba3f309b2549a2e1683a3e0f2
Žygimantas Kadiša <kadisa917@gmail.com>
2020-04-25 17:26:16 +0300

    Initial commit

---
+++ Drones/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Drones.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
