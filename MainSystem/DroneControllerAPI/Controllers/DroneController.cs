f74b94e92113dab94d68395db4b41def04acc117
Žygimantas Kadiša <kadisa917@gmail.com>
2020-05-22 22:38:47 +0300

    Finished license plate recognition

--- MainSystem/DroneControllerAPI/Controllers/DroneController.cs
+++ MainSystem/DroneControllerAPI/Controllers/DroneController.cs
@@ -4,9 +4,13 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace DroneControllerAPI.Controllers
 {
@@ -42,21 +46,47 @@ namespace DroneControllerAPI.Controllers
             using (var ms = new MemoryStream(buffer))
             {
                 img = Image.FromStream(ms);
-                //string imgPath = Path.Combine(Directory.GetCurrentDirectory(), "image.jpeg");
-                //Debug.WriteLine("Saving image in: " + imgPath);
-                //img.Save(imgPath);
+                string imgPath = Path.Combine(Directory.GetCurrentDirectory(), "image.jpeg");
+                Debug.WriteLine("Saving image in: " + imgPath);
+                img.Save(imgPath);
+                GetLicensePlate(getBase64String(img));
             }
-
 
             Debug.WriteLine("Ending post");
             return "succ";
         }
 
-        private void GetLicensePlate()
+        private string getBase64String(Image img)
         {
+            using (MemoryStream m = new MemoryStream())
+            {
+                img.Save(m, img.RawFormat);
+                byte[] imageBytes = m.ToArray();
 
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+        private void GetLicensePlate(string base64)
+        {
             string secret_key = "sk_e78b13b44b250ebc3d9f61ff";
-            string url = string.Format("https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=eu&secret_key={1}", secret_key);
+            string url = string.Format("https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=eu&secret_key={0}", secret_key);
+
+            Thread t = new Thread(async () =>
+            {
+                var client = new HttpClient();
+                StringContent content = new StringContent(base64);
+                HttpResponseMessage reponse = await client.PostAsync(url, content);
+                byte[] res = await reponse.Content.ReadAsByteArrayAsync();
+                string json = Encoding.UTF8.GetString(res);
+
+                string plate = (string)JObject.Parse(json)["results"][0]["plate"];
+
+                Debug.WriteLine(plate);
+            });
+            t.Start();
         }
 
         public static byte[] ReadToEnd(System.IO.Stream stream)
